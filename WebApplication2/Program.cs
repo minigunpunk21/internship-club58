644c1baa0827f6ecb9352f67bd182279adddbbe6
minigunpunk21 <error.danilov5@mail.ru>
2024-03-06 23:20:02 +0300

    как исправить блина

--- WebApplication2/Program.cs
+++ WebApplication2/Program.cs
@@ -13,7 +13,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Your API Title v1");
+                    options.RoutePrefix = string.Empty;
+                });
 }
 
 app.UseHttpsRedirection();
